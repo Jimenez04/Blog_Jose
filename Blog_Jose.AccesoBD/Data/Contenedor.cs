df3a5f2e64fb3e48d8da75128f7cd26cbbf1b083
José Enrique Jiménez Soto <jose.jimenezsoto18@ucr.ac.cr>
2021-02-24 19:52:27 -0600

    Terminacion de Repos

---
+++ Blog_Jose.AccesoBD/Data/Contenedor.cs
@@ -0,0 +1,32 @@
+﻿using Blog_Jose.AccesoBD.Data.Repositorio;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Blog_Jose.AccesoBD.Data
+{
+    public class Contenedor : IContenedor
+    {
+
+        private readonly ApplicationDbContext context;
+
+        public Contenedor(ApplicationDbContext context)
+        {
+            this.context = context;
+            categoria = new CategoriaRepositorio(context);
+
+        }
+
+        public ICategoriaRepositorio categoria { get; private set; }
+
+        public void Dispose()
+        {
+            context.Dispose();
+        }
+
+        public void Save()
+        {
+            context.SaveChanges();
+        }
+    }
+}
---
+++ Blog_Jose.AccesoBD/Data/Repositorio/IContenedor.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Blog_Jose.AccesoBD.Data.Repositorio
+{
+   public interface IContenedor : IDisposable
+    {
+        ICategoriaRepositorio categoria { get; }
+
+
+        void Save();
+    }
+}
